03779508ae3211a914b51461effbc914a34cf0f1
Akanksha Batwal <akankshabatwal54@gmail.com>
2026-09-09 22:36:12 +0530

    Implement new line printing using println
    Implemented a Java program to demonstrate new line printing using println().
    Each string is displayed on a separate line in the output.

---
+++ NewLine.java
@@ -0,0 +1,7 @@
+public class NewLine{
+    public static void main(String[] args){
+        System.out.println("Geeks");
+        System.out.println("for");
+        System.out.println("Geeks");
+    }
+}
